918c8ce17b50017acfff2805a435e6d35d9e2272
Luizettp <dado7@uol.com.br>
2023-04-19 17:08:25 -0300

    Adicao do arquivo do jogo

---
+++ Batalha_naval.py
@@ -0,0 +1,39 @@
+# Define posições 
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    posicao_ocupada = []
+    i = 0
+    while i < tamanho:
+        if orientacao == 'vertical':
+            posicao_ocupada.append([linha+i,coluna]) 
+            i+=1
+        else:
+            posicao_ocupada.append([linha,coluna+i])
+            i+=1
+    return posicao_ocupada
+
+
+# Prenchee Frota
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicao_ocupada = []
+    i = 0
+    while i < tamanho:
+        if orientacao == 'vertical':
+            posicao_ocupada.append([linha+i,coluna]) 
+            i+=1
+        else:
+            posicao_ocupada.append([linha,coluna+i])
+            i+=1
+    if nome_navio in frota:
+        frota[nome_navio].append(posicao_ocupada)
+    else:
+        frota[nome_navio] = [posicao_ocupada]
+    return frota
+
+
+# Faz Jogada
+def faz_jogada (tabuleiro, linha, coluna):
+    if tabuleiro [linha][coluna] == 1:
+        tabuleiro [linha][coluna] = 'X'
+    else:
+        tabuleiro [linha][coluna] = '-'
+    return tabuleiro
